d776e325a3f9ed03300d6c577570cf37d39cce16
Jeff Stewart <777752+object01@users.noreply.github.com>
2025-02-18 08:50:42 -0500

    Addressing Fab submission issues.

--- Source/UnlitShadedViewMode/UnlitShadedViewMode.Build.cs
+++ Source/UnlitShadedViewMode/UnlitShadedViewMode.Build.cs
@@ -1,4 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2025, Jeff Stewart
+// Email: object01@gmail.com
+// All rights reserved.
+//
+// This software is provided "as is," without warranty of any kind,
+// express or implied, including but not limited to the warranties
+// of merchantability, fitness for a particular purpose, and
+// noninfringement. In no event shall the author be liable for any
+// claim, damages, or other liability, whether in an action of
+// contract, tort, or otherwise, arising from, out of, or in
+// connection with the software or the use or other dealings in
+// the software.
 
 using UnrealBuildTool;
 
